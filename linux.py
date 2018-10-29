399edc945470dbec37591df151f0400d59d780b7
yuhongd <dongyuhong163@163.com>
2018-10-29 17:57:11 +0800

    增加Linux快捷键

--- linux.py
+++ linux.py
@@ -6,3 +6,9 @@
 控制台复制粘贴（vim编辑过程中）
 复制：ctrl+insert
 粘贴：shift+insert
+
+###linux终端打开图片的方法
+$eog test.png
+#eog （eye of gmone，是linux下内置的图片查看器。ubuntu16.04）
+#fbi （ubuntu16.04下需安装：apt install fbi）
+#fibda
