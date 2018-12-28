cae0580790f5c3f96402d8851fdeef790c04faab
root <root@DESKTOP-32M1DEH.localdomain>
2018-12-28 19:15:52 +0800

    add tmux

---
+++ linux_tools.py
@@ -0,0 +1,9 @@
+# tmux
+# 安装方式
+$ apt-get install tmux
+# 分屏设置：
+上下分屏：              ctrl+b 再按 ”
+左右分屏：              ctrl+b 再按 %
+切换屏幕：              ctrl+b 再按 o
+关闭一个终端：          ctrl+b 再按x
+上下分屏与左右分屏切换：ctrl+b 再按空格
